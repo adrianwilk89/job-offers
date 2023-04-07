706fbe5a054c283cd2cd37f9d441414ddeb45c3f
Adrian <adrian.przybyla@o2.pl>
2023-04-07 03:03:34 +0200

    handle getJobOffers error

--- src/app/modules/job-offers/job-offers.service.ts
+++ src/app/modules/job-offers/job-offers.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError, catchError } from 'rxjs';
 import { JobOffer } from './constants';
 import { environment } from 'src/environments/environment';
 
@@ -10,7 +10,9 @@ export class JobOffersService {
     constructor(private http: HttpClient) {}
 
     getJobOffers(): Observable<{ jobs: JobOffer[] }> {
-        return this.http.get<{ jobs: JobOffer[] }>(`${environment.apiEndpoint}offers/list/pl`)
+        return this.http.get<{ jobs: JobOffer[] }>(`${environment.apiEndpoint}offers/list/pl`).pipe(catchError((error) => {
+            return throwError(() => error);
+        }),)
     }
 
 }
